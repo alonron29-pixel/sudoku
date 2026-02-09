69e82353562dc59631f246829f38936ac8e029fa
Alon Ron <alonron29@gmail.com>
2026-02-10 01:10:22 +0200

    added UndoStep class

---
+++ SudokuProject/SudokuProject/UndoStep.cs
@@ -0,0 +1,12 @@
+﻿public struct UndoStep
+{
+    public int CellIdx;
+    public long RemovedBit;
+
+    public const short ValueAssignmentFlag = 0;
+
+    public UndoStep(int cellIdx, long removedBit)
+    {
+        CellIdx = cellIdx;
+        RemovedBit = removedBit;
+    }
