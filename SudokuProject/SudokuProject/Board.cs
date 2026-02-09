679ddad326e6f1e1bd3046ae889ab56a5ea644b7
Alon Ron <alonron29@gmail.com>
2026-02-10 00:56:58 +0200

    added using statments

--- SudokuProject/SudokuProject/Board.cs
+++ SudokuProject/SudokuProject/Board.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
 
 public class Board
 {
