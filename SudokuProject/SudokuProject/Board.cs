0818e5a7e0adac78140bba0a0912ae5219bf6127
Alon Ron <alonron29@gmail.com>
2026-02-10 00:24:00 +0200

    board fields and constructor

---
+++ SudokuProject/SudokuProject/Board.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Board
+{
+    public int Size { get; private set; }
+    
+    public int TotalCells { get; private set; } 
+
+    // A single continuous block of memory containing all neighbors indexes for all cells.
+    public int[] AllNeighbors { get; private set; }
+
+    // Starting index in _allNeighbors for each cell.
+    public int[] NeighborOffsets { get; private set; }
+
+    public int NeighborsPerCell { get; private set; }
+    
+    public Cell[] Cells { get; private set; }
+
+    private int _boxRows;
+    private int _boxCols;
+    private long _fullMask;
+
+    private int[] _workStack;
+    private int _workPtr = 0;
+
+    private const char EmptyCellCharValue = '0';
+    private readonly string _symbols = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    public Dictionary<char, int> CharToVal { get; private set; }
+    public Dictionary<int, char> ValToChar { get; private set; }
+
+    public Board(string boardString)
+    {
+        InitializeDimensions(boardString);
+        InitializeDictionaries();
+        BuildNeighborsAndInitCells(boardString);
+    }
