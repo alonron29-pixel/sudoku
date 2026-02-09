f8b1b9a4a17015cc6ef1e0acb4b06eb7b83ef892
Alon Ron <alonron29@gmail.com>
2026-02-10 01:18:00 +0200

    added backtrack func

--- SudokuProject/SudokuProject/Solver.cs
+++ SudokuProject/SudokuProject/Solver.cs
@@ -25,4 +25,75 @@ public class Solver
 
         _undoPtr = 0;
     }
+	
+	/// <summary>
+    /// Executes the full solving process by performing an initial constraint propagation 
+    /// followed by a recursive backtracking search.
+    /// </summary>
+    public bool Solve()
+    {
+        _board.InitialPropagation();
+        return Backtrack();
+    }
+
+    /// <summary>
+    /// Converts a single-bit bitmask into its corresponding Sudoku integer value
+    /// </summary>
+    public static int BitmaskToValue(long bitmask)
+    {
+        int count = 0;
+        while (bitmask > 0)
+        {
+            bitmask >>= 1;
+            count++;
+        }
+        return count;
+    }
+
+    // <summary>
+    /// The core recursive algorithm that explores potential cell values. 
+    /// Uses a snapshot-based undo system to revert changes during failed branches.
+    /// </summary>
+    private bool Backtrack()
+    {
+        for (int i = 0; i < _board.TotalCells; i++)
+        {
+            if (_board.Cells[i].Value == Cell.EmptyCellValue && _board.Cells[i].CandidatesCount == 0)
+                return false;
+        }
+
+        int cellIndex = FindBestCellIndex();
+        
+        if (cellIndex == FullBoardFlag)
+        {
+            return true;
+        }
+
+        int snapshot = _undoPtr;
+  
+        ref Cell currentCell = ref _board.Cells[cellIndex];
+        for (int val = 1; val <= _board.Size; val++)
+        {
+            // Create a bitmask for the current value 
+            long valBit = 1L << (val - 1);
+            // Check if the current value is a candidate for this cell
+            if ((currentCell.CandidatesMask & valBit) != 0)
+            {
+                currentCell.Value = val;
+                _undoStack[_undoPtr++] = new UndoStep(cellIndex, UndoStep.ValueAssignmentFlag);
+              
+                if (TryValue(cellIndex))
+                {
+                    if (Backtrack())
+                    {
+                        return true; // got to a solution
+                    }
+                }
+                
+                Undo(snapshot);
+            }
+        }
+    
+        return false; //no option is valid, backtrack to previous call
+    }
 }
