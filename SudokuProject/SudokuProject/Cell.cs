fe7a49445522e3746bd6628ff3879d32879fb9c6
Alon Ron <alonron29@gmail.com>
2026-02-10 01:07:26 +0200

    added cell class

---
+++ SudokuProject/SudokuProject/Cell.cs
@@ -0,0 +1,20 @@
+﻿public class Cell
+{
+    public int Value { get; set; }
+    public long CandidatesMask { get; set; } // bits representing the options of the value for this cell
+    public int CandidatesCount { get; set; }  // number of on bits
+
+    public const int EmptyCellValue = 0;
+
+    public Cell(int value, long mask=0, int count=0)
+    {
+        Value = value;
+        CandidatesMask = mask;
+        CandidatesCount = count;
+    }
+    public void RemoveCandidate(long candidateMask)
+    {
+        CandidatesMask &= ~candidateMask;
+        CandidatesCount--;
+    }
+}
